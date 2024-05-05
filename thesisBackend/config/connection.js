a03c49a07e9d7186fe27aa9e25de66e258197206
unknown <farrel.ahmad.zaidan@gmail.com>
2024-05-05 21:59:45 +0700

    update on backend try to add connection to database

---
+++ thesisBackend/config/connection.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require('sequelize');
+const config = require('./config.json');
+
+const db = (database) => {
+  const server = {
+    database: config[database].database,
+    username: config[database].username,
+    password: config[database].password,
+    config: config[database]
+  };
+
+  return new Sequelize(server.database, server.username, server.password, server.config);
+}
+
+exports.checkDatabaseConnection = async (sequelize) => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully.');
+  } catch (error) {
+    console.error('Error connecting to database:', error.message);
+  }
+};
+
+// MYSQL
+exports.test = db('test');
+exports.production = db('production');
--- thesisBackend/routes/index.js
+++ thesisBackend/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var { test, checkDatabaseConnection } = require('../connection'); // Assuming the connection.js file is one level up
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try {
+    await checkDatabaseConnection(test); // Check database connection
+
+    res.render('index', { title: 'Express', database_connected: true }); // Pass database_connected status to the view
+  } catch (error) {
+    console.error('Error connecting to database:', error.message);
+    res.render('index', { title: 'Express', database_connected: false }); // Pass database_connected status to the view
+  }
 });
 
 module.exports = router;
