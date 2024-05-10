0c7dd953c92778c3f0d43c2748349cf8a537f498
unknown <farrel.ahmad.zaidan@gmail.com>
2024-05-10 22:29:40 +0700

    add mape

--- thesisBackend/controllers/arima-controller.js
+++ thesisBackend/controllers/arima-controller.js
@@ -1,9 +1,6 @@
 const { log } = require('console');
 const { HistoryTab } = require('../models/mst-history')
 const fs = require('fs')
-// const pd = require('pandas-js').default; 
-// const { ARIMA } = require('arima');
-// const np = require('numpy-js');
 
 module.exports = {
     index: async (req, res) => {
@@ -18,50 +15,56 @@ module.exports = {
 
     arimatest: async (req, res) => {
         try {
-          const historyData = await HistoryTab.findAll({
-            where: {
-              area_name: 'OCI1',
-              device_name: 'CAP - FEEDER C/V 1',
-              test_name: '2H',
-            }, 
-            attributes: ['do_date', 'device_name', 'value'],
-            order: [['do_date', 'ASC']], // Ensure data is sorted by dateas
-          });
-    
-          const dates = [];
-          const values = [];
-    
-          historyData.forEach(item => {
-            dates.push(item.do_date);
-            values.push(item.value);
-          });
-    
-          // Calculate average value to use as the forecast
-          const averageValue = values.reduce((acc, curr) => acc + curr, 0) / values.length;
-    
-          const forecastValues = [];
-          const forecastDates = [];
-    
-          // Assuming a simple forecast where each subsequent value is the average of previous values
-          for (let i = 0; i < values.length; i++) {
-            forecastValues.push(averageValue);
-            forecastDates.push(dates[i]);
-          }
-    
-          const responseData = {
-            'forecast_values': forecastValues,
-            'forecast_dates': forecastDates,
-            'original_values': values,
-          };
-    
-          // Return JSON response
-          res.status(200).json(responseData);
+            const historyData = await HistoryTab.findAll({
+                where: {
+                    area_name: 'OCI1',
+                    device_name: 'CAP - FEEDER C/V 1',
+                    test_name: '2H',
+                },
+                attributes: ['do_date', 'device_name', 'value'],
+                order: [['do_date', 'ASC']], // Ensure data is sorted by date
+            });
+
+            const dates = [];
+            const values = [];
+
+            historyData.forEach(item => {
+                dates.push(item.do_date);
+                values.push(item.value);
+            });
+
+            // Calculate average value to use as the forecast
+            const averageValue = values.reduce((acc, curr) => acc + curr, 0) / values.length;
+
+            const forecastValues = [];
+            const forecastDates = [];
+
+            // Assuming a simple forecast where each subsequent value is the average of previous values
+            for (let i = 0; i < values.length; i++) {
+                forecastValues.push(averageValue);
+                forecastDates.push(dates[i]);
+            }
+
+            // Calculate MAPE
+            const mapeValues = values.map((actual, index) => Math.abs((actual - forecastValues[index]) / actual));
+            const mape = (mapeValues.reduce((acc, curr) => acc + curr, 0) / mapeValues.length) * 100;
+
+            const responseData = {
+                'forecast_values': forecastValues,
+                'forecast_dates': forecastDates,
+                'original_values': values,
+                'mape': mape,
+            };
+
+            // Return JSON response
+            res.status(200).json(responseData);
         } catch (error) {
-          const errorMessage = `Failed to fetch data from the database or perform ARIMA prediction: ${error.toString()}`;
-          console.log(errorMessage);
-          res.status(500).json({ error: errorMessage });
+            const errorMessage = `Failed to fetch data from the database or perform ARIMA prediction: ${error.toString()}`;
+            console.log(errorMessage);
+            res.status(500).json({ error: errorMessage });
         }
-      }
+    }
+
 
     // findById: async (req, res) => {
     //     try {
@@ -85,14 +88,14 @@ module.exports = {
     //             duration: req.body.duration, 
     //             color: req.body.color,
     //         });
-    
+
     //         res.status(200).json(activity);
     //     } catch (error) {
     //         console.error(error);
     //         res.status(500).json(error);
     //     }
     // },
-  
- 
+
+
 
 }
