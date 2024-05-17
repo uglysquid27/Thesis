d4c037ab1bc32fe4c3333973c7b1d70ea2fb2685
unknown <farrel.ahmad.zaidan@gmail.com>
2024-05-17 19:40:23 +0700

    done monte carlo calculation on testing

---
+++ thesisBackend/routes/monte-carlo.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+// const moment = require("moment");
+// const path = require("path");
+// const multer = require("multer");
+var monteCarloController = require("../controllers/monte-carlo-controller")
+
+/* GET home page. */
+router.get('/', monteCarloController.index);
+router.get('/testing', monteCarloController.montecarlotest);
+
+
+
+module.exports = router;
