97a96fc84ba69bfc2c4f2fcd93fa2e21f6cd142e
unknown <farrel.ahmad.zaidan@gmail.com>
2024-06-08 22:04:25 +0700

    update

--- thesisBackend/config/connection.js
+++ thesisBackend/config/connection.js
@@ -16,7 +16,7 @@ exports.checkDatabaseConnection = async (sequelize) => {
   try {
     await sequelize.authenticate();
     console.log('Database connected successfully.');
-  } catch (error) {
+  } catch (error) { 
     console.error('Error connecting to database:', error.message);
   } 
 };
